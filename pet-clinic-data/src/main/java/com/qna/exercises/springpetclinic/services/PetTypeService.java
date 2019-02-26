d9865f596e3bee0d34e880ee4ae4b68554c6cecd
Anna Kuna <ana.kuna@gmail.com>
2019-02-26 18:13:11 +0100

    added PetType Service

---
+++ pet-clinic-data/src/main/java/com/qna/exercises/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.qna.exercises.springpetclinic.services;
+
+import com.qna.exercises.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
