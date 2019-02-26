277465713008955cba35af5317d2ae0d7b0be625
Anna Kuna <ana.kuna@gmail.com>
2019-02-26 19:13:55 +0100

    added Specialities Map Service Impl

---
+++ pet-clinic-data/src/main/java/com/qna/exercises/springpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.qna.exercises.springpetclinic.services;
+
+import com.qna.exercises.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
